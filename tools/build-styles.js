6d52ed77f0ebd5bf9b4e62357857ca09c51c308d
rodriandrade <rodriandrade1498@gmail.com>
2021-08-03 00:47:46 -0300

    update

--- tools/build-styles.js
+++ tools/build-styles.js
@@ -4,13 +4,13 @@ const { exec } = require('child_process');
 
 const run = promisify(exec);
 
-run('rm -rf .tmp/')
+run('del /s /q .tmp')
   .then(() => run('node-sass -o .tmp/ src/styles.scss'))
   .then(() => run('postcss .tmp/styles.css --use autoprefixer --no-map -d .tmp/'))
-  .then(() => run('mv .tmp/styles.css .tmp/styles-compiled.css'))
-  .then(() => run('mkdir -p es lib'))
-  .then(() => run('cp .tmp/styles-compiled.css es/ && cp .tmp/styles-compiled.css lib/'))
-  .then(() => run('cp src/styles.scss es/ && cp src/styles.scss lib/'))
+  .then(() => run(`rename .tmp\\styles.css styles-compiled.css`))
+  //.then(() => run('mkdir -p es lib'))
+  .then(() => run('copy .tmp\\styles-compiled.css es\\ && copy .tmp\\styles-compiled.css lib\\'))
+  .then(() => run('copy src\\styles.scss es\\ && copy src\\styles.scss lib\\'))
   .then(() => console.log('✔ Styles have been build'))
   .catch(err => {
     console.error(err);
