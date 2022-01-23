8e315d290ea9f1bfa35cbc7bc58374ab68baa381
rodriandrade <rodriandrade1498@gmail.com>
2022-01-23 02:54:37 -0300

    build styles change

--- tools/build-styles.js
+++ tools/build-styles.js
@@ -4,6 +4,18 @@ const { exec } = require('child_process');
 
 const run = promisify(exec);
 
+run('rm -rf .tmp/')
+  .then(() => run('node-sass -o .tmp/ src/styles.scss'))
+  .then(() => run('postcss .tmp/styles.css --use autoprefixer --no-map -d .tmp/'))
+  .then(() => run('mv .tmp/styles.css .tmp/styles-compiled.css'))
+  .then(() => run('mkdir -p es lib'))
+  .then(() => run('cp .tmp/styles-compiled.css es/ && cp .tmp/styles-compiled.css lib/'))
+  .then(() => run('cp src/styles.scss es/ && cp src/styles.scss lib/'))
+  .then(() => console.log('✔ Styles have been build'))
+  .catch(err => {
+    console.error(err);
+});
+/*
 run('del /s /q .tmp')
   .then(() => run('node-sass -o .tmp/ src/styles.scss'))
   .then(() => run('postcss .tmp/styles.css --use autoprefixer --no-map -d .tmp/'))
@@ -15,3 +27,4 @@ run('del /s /q .tmp')
   .catch(err => {
     console.error(err);
   });
+*/
